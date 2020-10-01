41dee08adf5fbb6ad6cf8a6615c7c638a275bb31
James Yeates <jimibue@gmail.com>
2020-09-30 19:37:09 -0600

    Updates

---
+++ precache-manifest.d30166f2704576791522b079d3eb44a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cf4dcee28758445c4fd07ffe3dcc77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a74299e8709c1dfb81f",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "9829d70609fb469474bd",
+    "url": "./static/js/2.5fad79fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5fad79fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a74299e8709c1dfb81f",
+    "url": "./static/js/main.7fb05f13.chunk.js"
+  },
+  {
+    "revision": "904637c740b7ffa90574",
+    "url": "./static/js/runtime-main.43d8cf59.js"
+  }
+]);
